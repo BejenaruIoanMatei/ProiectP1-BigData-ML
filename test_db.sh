3872f52f9df086547e1874ee1571c9eda6937769
BejenaruIoanMatei <bejematei@gmail.com>
2026-03-18 15:40:30 +0200

    Docker: mongodb setup and connection test

--- test_db.sh
+++ test_db.sh
@@ -4,3 +4,6 @@
 export $(grep -v '^#' .env | xargs)
 echo "Connecting to $POSTGRES_DB as $POSTGRES_USER..."
 docker-compose exec postgres psql -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT 'Connection Successful!' as Status;"
+
+echo "Connecting to mongodb as $MONGO_USER..."
+docker-compose exec mongodb mongosh -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.adminCommand('ping')"
